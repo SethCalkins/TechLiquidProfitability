5db609415bb3adbfa23b62519ac2795b2c5b0ec9
Jason Ashton <ashtonguitars@gmail.com>
2014-11-27 14:22:58 -0500

    Improved Search

--- techliquidchecker.py
+++ techliquidchecker.py
@@ -7,15 +7,15 @@ from bs4 import BeautifulSoup
 import urllib.request
 import csv
 
-html_doc = urllib.request.urlopen('http://techliquidators.com/index.cfm?p=4&sorttype=11&categories=0&sizes=1&condition=0&location=0&vhr=&vhi=&bc=1&maxrows=100')
-soup = BeautifulSoup(html_doc)
+mainlink = urllib.request.urlopen('http://techliquidators.com/index.cfm?p=4&sorttype=11&categories=0&sizes=1&condition=0&location=0&vhr=&vhi=&bc=1&maxrows=100')
+mainlink = BeautifulSoup(mainlink)
 count = 1
-prevlink = 0
+prevlink = ''
 
 with open('output.csv', 'w', newline='') as csvfile: ##opens output.csv as a writable file
     csvwriter = csv.writer(csvfile, dialect='excel') ##CSVwriter as excel
     csvwriter.writerow(['Auction Name', 'Current Bid', 'Quantity','Product','MSRP','Market', 'Revenue', 'Auction Revenue', 'Profit', '', 'Auction Link', 'Market Link']) ## makes headers
-    for link in soup.find_all('a'): #finds all links
+    for link in mainlink.find_all('a'): #finds all links
         if ((str(link.get('href'))).find('index.cfm/p/34')) != -1 and link.get('href') != prevlink: ##makes sure it fits criteria, then does all that
             prevlink = link.get('href')
             
@@ -26,21 +26,20 @@ with open('output.csv', 'w', newline='') as csvfile: ##opens output.csv as a wri
             auctionname = str(auctionname)[7:len(auctionname) - 9] ##finds the auction name
             auctionname = str(auctionname).replace('&amp;', '+')
             auctionname = str(auctionname).replace(';','')
-            ##currentbid = str(man)[str(man).find('Current Bid:'):]
-            currentbid = str(man)[str(man).find('Current Bid:') + 170:str(man).find('Current Bid:') + 176]
-           
-            
-            manifest = str(man)[str(man).find('name="urlManifestHTM" type="hidden" value="'):str(man).find('.htm">',str(man).find('name="urlManifestHTM" type="hidden" value="'))]##extracts manifest link
-            manifest_trimmed = 'http://techliquidators.com' + str(manifest)[str(manifest).find('/'):] + '.htm' ## finalizes manifest link
-            
-            print ('\n' + str(count) + ". " + str(manifest_trimmed)) ##lists link with numbers
-            count = count + 1
-            print ('Current Bid: ' + str(currentbid))
 
-            manifest_downloaded = BeautifulSoup(urllib.request.urlopen(manifest_trimmed)) ##download and put into beautiful soup the manifest
+            currentbid = str(man)[str(man).find('Current Bid:') + 170:str(man).find('Current Bid:') + 176]
+            print ('\nCurrent Bid: ' + str(currentbid))
+
+            manifest = str(man)[str(man).find('id="urlManifestHTM"') + 63:]##extracts manifest link
+            manifest = str(manifest)[:str(manifest).find('.htm') + 4]
+            manifest_trimmed = 'http://techliquidators.com' + str(manifest) ## finalizes manifest link
+
+            count = count + 1
+
+            manifest_downloaded = urllib.request.urlopen(manifest_trimmed)
+            manifest_downloaded = BeautifulSoup(manifest_downloaded) ##download and put into beautiful soup the manifest
             manifest_downloaded.prettify()
             manifest_extract = manifest_downloaded.find_all('td')
-
             manifest_extract = str(manifest_extract)[str(manifest_extract).find('Total MSRP') + 17:] ##delete preliminary info
 
             auctionrevenue = 0 ##reset auction revenue
@@ -51,11 +50,13 @@ with open('output.csv', 'w', newline='') as csvfile: ##opens output.csv as a wri
                 quantity = str(manifest_extract)[4:str(manifest_extract).find('>, ') - 4]
                 manifest_extract = str(manifest_extract)[str(manifest_extract).find('>,') + 3:]
                 manifest_extract = str(manifest_extract)[str(manifest_extract).find('>,') + 3:]
+
+                partnum = str(manifest_extract)[4:str(manifest_extract).find('>, ') - 4]
+
                 manifest_extract = str(manifest_extract)[str(manifest_extract).find('>,') + 3:]
 
                 name = str(manifest_extract)[4:str(manifest_extract).find('>, ') - 4] ## prints the listing name
-                name = str(name).replace('AT&amp;T', 'ATT')
-                name = str(name).replace(';', ' ')
+                name = str(name).replace('AT&amp;T', 'ATT').replace(';', ' ')
 
                 manifest_extract = str(manifest_extract)[str(manifest_extract).find('>,') + 3:]
                 manifest_extract = str(manifest_extract)[str(manifest_extract).find('>,') + 3:]
@@ -68,38 +69,30 @@ with open('output.csv', 'w', newline='') as csvfile: ##opens output.csv as a wri
                 
                 ##wall of replacements
                 itemname = name.lower()
-                itemname = str(itemname).replace('-', '+')
-                itemname = str(itemname).replace('at&amp;t;', 'att')
-                itemname = str(itemname).replace('"', '+')
-                itemname = str(itemname).replace('feat.', '+')
-                itemname = str(itemname).replace('/', '+')
-                itemname = str(itemname).replace(' ', '+')
-                if 'ipad' in str(itemname): 
+                itemname = str(itemname).replace('-', '+').replace('at&amp;t;', 'att').replace('"', '+').replace('feat.', '+').replace('/', '+').replace(' ', '+')
+                if 'ipad' in str(itemname):
                     itemname = str(itemname)[str(itemname).find('ipad'):] ##deletes model number prefix
+                if 'intel core' in str(itemname) or 'amd' in str(itemname):
+                    itemname = str(partnum) ##if a laptop, the part number is used instead of item name
                 try:
                     listingurl = ('http://www.thepricegeek.com/results/' + str(itemname) + '?country=us')
                     marketprice = BeautifulSoup(urllib.request.urlopen(listingurl))
                     marketprice = str((marketprice.find_all(class_="median")))[20:27]
-                    marketprice = str(marketprice).replace('$','')
-                    marketprice = str(marketprice).replace('<','')
-                    marketprice = str(marketprice).replace('/','') ##clean up price
+                    marketprice = str(marketprice).replace('$','').replace('<','').replace('/','') ##clean up price
 
                     if(str(marketprice) == ''):
                         marketprice = 'NOTFOUND'
-                        ##print('http://www.thepricegeek.com/results/' + str(itemname) + '?country=us')
                 except urllib.error.HTTPError:
                     marketprice = 'HTTPERROR'
-                    ##print('http://www.thepricegeek.com/results/' + str(itemname) + '?country=us')
                 except UnicodeEncodeError:
-                    ##print('http://www.thepricegeek.com/results/' + str(itemname) + '?country=us')
-                    break ##i dont know
+                    marketprice = 'HTTPERROR'
                 except urllib.error.URLError:
-                    break
+                    marketprice = 'HTTPERROR'
                 try:
-                    if(marketprice != 'NOTFOUND'):
+                    if(marketprice != 'NOTFOUND' and marketprice != 'NOTFOUND'):
                         itemrevenue = int(quantity) * float(marketprice)
                         auctionrevenue = auctionrevenue + itemrevenue
-                    profit = auctionrevenue - int(currentbid)
+                        profit = auctionrevenue - int(currentbid) + 2
                 except ValueError:
                     print('Error')
                 
