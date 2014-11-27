a4d14b75e82e20a9de9fd30b658a0edd6ea9040b
Jason Ashton <ashtonguitars@gmail.com>
2014-11-27 11:12:46 -0500

    Added version check

--- techliquidchecker.py
+++ techliquidchecker.py
@@ -1,3 +1,8 @@
+import sys
+if sys.version_info<(2,6,0):
+  sys.stderr.write("You need python 2.6 or later to run this script\n")
+  exit(1)
+
 from bs4 import BeautifulSoup
 import urllib.request
 import csv
